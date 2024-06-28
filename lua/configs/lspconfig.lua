5b00b0382987d32dfb68d809e7a067919fe14d66
John Oye <olugbengajohnoye@gmail.com>
2024-06-28 23:40:27 +0100

    Set up PHP LSP and TS LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "phpactor" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -18,6 +18,13 @@ end
 -- typescript
 lspconfig.tsserver.setup {
   on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- phpactor
+lspconfig.phpactor.setup {
+  on_attach_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
 }
