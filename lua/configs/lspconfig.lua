045a5f7491e4d9d46e4c70c7713ec5f95fd651a8
johnoye742 <olugbengajohnoye@gmail.com>
2025-02-16 22:39:49 +0100

    made a lot of breaking changes

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "phpactor", }
+local servers = { "html", "cssls", "phpactor", "clangd", "rustanalyzer" }
 
 
 -- lsps with default config
