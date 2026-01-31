4eb67fb2997ecd340fb33f5713e8e3ac9c60eea4
John Oye <olugbengajohnoye@gmail.com>
2026-01-31 05:08:24 +0100

    Some breaking changes

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,25 +1,5 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+local servers = { "html", "cssls", "phpactor", "clangd", "rust_analyzer", "ts_ls" }
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "phpactor", "clangd", "rust_analyzer" }
-
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+for lsp in ipairs(servers) do
+  vim.lsp.enable(servers[lsp])
 end
-
--- typescript
-lspconfig.ts_ls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>tt", function ()
+  require("base46").toggle_transparency() end)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
